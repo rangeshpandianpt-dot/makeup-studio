2bef6523c8553715588a25d96d48765d6e038d64
RANGESH <rangeshpandianpt@gmail.com>
2026-06-06 17:16:59 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import react from "@vitejs/plugin-react";
+import tsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [
+    tanstackStart({
+      server: { entry: "server" },
+    }),
+    react(),
+    tailwindcss(),
+    tsConfigPaths({ projects: ["./tsconfig.json"] }),
+  ],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  server: { host: "::", port: 8080 },
+});
